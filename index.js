340290fca0369724b0678317c42f3760ba64b33f
Mwansasquared <mwansasquared9@domain.com>
2021-09-25 19:33:37 +0200

    Can't seem to figure out how to have nodejs load the external css file, inline styling is working just fine

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.set('views', path.join(__dirname + '/views'));
-app.set('view engine', 'pug');
-app.use(express.static('public'));
 //app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, '/public')));
+//app.use(express.static('public'));
 
-//app.use('/static', express.static(path.join(__dirname, '/public')));
 // In your html, note absolute path
 //href='./css/style.css'
 
